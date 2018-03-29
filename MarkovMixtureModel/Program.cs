fc2a029c6c182db97026bd2e77569ee964c06111
vladislavsd <vladislavsd@voicebox.com>
2018-03-29 16:18:08 -0700

    WIP: Add command-line parsing logic - getting verb (train or predict mode)

--- MarkovMixtureModel/Program.cs
+++ MarkovMixtureModel/Program.cs
@@ -10,6 +10,26 @@ namespace MarkovMixtureModel
     {
         static void Main(string[] args)
         {
+            var cmdInfo = new CommandLineInfo(args);
+
+            if (cmdInfo.Params.Length < 2)
+            {
+                PrintHelpMessage();
+                Environment.Exit(1);
+            }
+
+            // get verb
+            string verb = "";
+            if (cmdInfo.GotOption("--train"))
+                verb = cmdInfo.GetValue("--train");
+            if (cmdInfo.GotOption("--predict"))
+                verb = cmdInfo.GetValue("--predict");
+            if (verb == "")
+            {
+                PrintHelpMessage();
+                Environment.Exit(1);
+            }
+
             //
             // Set parameters: number of clusters
             //
@@ -48,6 +68,14 @@ namespace MarkovMixtureModel
             model.ObserveData(data, sizes, K);
             model.InitializeStatesRandomly();
             model.InferPosteriors();
+        }
+
+        static void PrintHelpMessage()
+        {
+            Console.WriteLine("Training:");
+            Console.WriteLine("\tUsage: MarkovMixtureModel.exe --train --data <data> --model <model>\n");
+            Console.WriteLine("Prediction:");
+            Console.WriteLine("\tUsage: MarkovMixtureModel.exe --predict --model <model> --data <data> --predictions <pred>\n");
         }
     }
 }
